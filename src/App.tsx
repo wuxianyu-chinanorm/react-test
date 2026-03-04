34aa6d2bb8b32150bb5953675a22eff035feb762
wuxianyu <wuxianyu@china-norm.com>
2026-03-04 15:39:53 +0800

    tmp

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 	return (
 		<>
 			<div>
-				xxxx
+				jafsdjfaksjf
 				<a href="https://vite.dev" target="_blank">
 					<img src={viteLogo} className="logo" alt="Vite logo" />
 				</a>
